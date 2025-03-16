e139765ac1a44ff26514bdfb973c2b09edc0a561
Anandaa69 <anandasinlapachote0@gmail.com>
2025-03-16 14:45:26 +0700

    add humi def eng model

--- pages/function_eng_model.py
+++ pages/function_eng_model.py
@@ -504,5 +504,232 @@ def eng_model_temp(days):
     # สมมุติว่า result คือ DataFrame ที่รวม predictions กับ pm_10
     result_plot_temp = pd.concat([result['prediction_label'].shift(-1)[start_date:new_date], real_data['temperature']], axis=1)
     result_plot_temp.columns = ['Predictions', 'Actual TEMP']  # ตั้งชื่อคอลัมน์
-    return result_plot_temp
     #---------------------------------------------
+    
+    return result_plot_temp
+
+def eng_model_predict_2_5(days):
+    num_days = days # วันที่ต้องการทำนาย
+    
+    import pycaret
+    import pandas as pd
+    import numpy as np
+    
+    # อ่านไฟล์ CSV
+    data = pd.read_csv('export_data/filtered_data_3_best.csv')
+    # ตั้ง 'timestamp' เป็น index หากยังไม่ได้ทำ
+    data['timestamp'] = pd.to_datetime(data['timestamp'])
+    data.set_index('timestamp', inplace=True)
+    
+    # ตั้งค่า freq ให้กับ index
+    data = data.asfreq('D')  # 'D' สำหรับรายวัน
+    
+    # เติมค่าที่หายไปด้วยการ interpolate แบบเส้นตรง
+    data.interpolate(method='linear', inplace=True)
+    
+    # กำหนดจำนวนวันที่ต้องการเพิ่ม
+    num_days = num_days
+    
+    # สร้างวันที่ใหม่
+    next_date = data.index[-1] + pd.Timedelta(days=1)
+    new_dates = pd.date_range(start=next_date, periods=num_days, freq='D')
+    
+    # สร้างแถวใหม่
+    new_rows = pd.DataFrame({'value': range(40, 40 + num_days)}, index=new_dates)
+    
+    # รวมข้อมูล (ป้องกัน index ซ้ำซ้อน)
+    data = pd.concat([data, new_rows]).sort_index()
+    
+    # ลบ index ที่ซ้ำกัน (ถ้ามี)
+    data = data.loc[~data.index.duplicated(keep='first')]
+    
+    last_date_temp = data.index[-1].strftime('%Y-%m-%d')
+    data["year"] = data.index.year
+    data["month"] = data.index.month
+    data["day"] = data.index.day
+    data["day_of_week"] = data.index.dayofweek  # 0 = Monday, 6 = Sunday
+    data["week_of_year"] = data.index.isocalendar().week  # ค่าจะเป็น int64
+    
+    # สมมติว่า data มี index เป็น datetime อยู่แล้ว
+    def assign_season(month):
+        if month in [2, 3, 4]:  # ก.พ. - เม.ย.
+            return "summer"        # ฤดูร้อน
+        elif month in [5, 6, 7]:  # พ.ค. - ก.ค.
+            return "early_rainy"   # ต้นฤดูฝน
+        else:  # ส.ค. - ม.ค.
+            return "late_rainy"    # ปลายฤดูฝน
+    
+    season_mapping = {'summer': 0, 'early_rainy': 1, 'late_rainy': 2}
+    data["season"] = data.index.month.map(assign_season)
+    data['season'] = data['season'].map(season_mapping)
+    
+    data["is_weekend"] = data.index.dayofweek.isin([5, 6]).astype(int)
+    
+    # สมมุติว่ามี DataFrame data ที่มีข้อมูลอยู่แล้ว
+    # เพิ่มฟีเจอร์ day_of_year
+    data['date'] = pd.to_datetime(data[['year', 'month', 'day']])
+    data['day_of_year'] = data['date'].dt.dayofyear
+    
+    # เพิ่มฟีเจอร์ month_sin และ month_cos
+    data['month_sin'] = np.sin(2 * np.pi * data['month'] / 12)
+    data['month_cos'] = np.cos(2 * np.pi * data['month'] / 12)
+    
+    # เพิ่มฟีเจอร์ day_of_week_sin และ day_of_week_cos
+    data['day_of_week_sin'] = np.sin(2 * np.pi * data['day_of_week'] / 7)
+    data['day_of_week_cos'] = np.cos(2 * np.pi * data['day_of_week'] / 7)
+    
+    data.drop(columns=['pm_10', 'pm_2_5_sp', 'value'], inplace=True)
+    data_temp = data.copy()
+    
+    data_temp.drop(columns=['humidity', 'pm_2_5'], inplace=True)
+    
+    lag_size = 7
+    for i in range(1,lag_size+1):
+        data_temp[f'temperature_lag_{i}'] = data_temp['temperature'].shift(i)
+    data_filtered = data_temp.loc['2023-09':]  # ดึงข้อมูลตั้งแต่กันยายน 2023 เป็นต้นไป
+    from pycaret.regression import load_model, predict_model
+    
+    model_temp = load_model("main/lgmb_temp_model_eng")
+    
+    data_compare = data_filtered.loc[:'2025-01-12']
+    result = pd.DataFrame()
+    
+    start_date = "2025-01-13"
+    end_date = last_date_temp
+    
+    model = model_temp
+    
+    for date in pd.date_range(start=start_date, end=end_date, freq="D"):
+        # Drop columns
+        test_1 = data_filtered
+        # ดึงวันนี้มา
+        test_1 = test_1.loc[str(date.date())]
+    
+        # ใส่เข้าไป
+        data_compare = pd.concat([data_compare, test_1], axis=0)
+    
+        for i in range(1,lag_size+1):
+            data_compare[f'temperature_lag_{i}'] = data_compare['temperature'].shift(i)
+    
+        # ดึง
+        test_1 = (data_compare.loc[str(date.date())]).drop(columns=['temperature'])
+        
+        # Drop ทิ้งล่าสุดใน data_compare
+        data_compare = data_compare.drop(data_compare.last_valid_index())
+        test_1 = test_1[~test_1.index.duplicated(keep='last')]
+    
+        # Predict
+        predict_test_1 = predict_model(model, data=test_1)
+    
+        # เพิ่มผลทำนายเข้าไปใน data_compare
+        data_compare = pd.concat([data_compare, predict_test_1], axis=0)
+    
+        # แทนที่ค่าในคอลัมน์ 'pm_2_5' ในวันที่ '2025-01-13' ด้วยค่าในคอลัมน์ 'prediction_label'
+        data_compare.loc[str(date.date()), 'temperature'] = data_compare.loc[str(date.date()), 'prediction_label']
+    
+        # ลบคอลัมน์ 'prediction_label' (หากต้องการแค่ผลทำนาย)
+        data_compare = data_compare.drop(columns=['prediction_label'])
+    
+        # กรองเอาวันที่ซ้ำกัน (index ซ้ำ) ออกให้เหลือแค่ตัวเดียว
+        data_compare = data_compare[~data_compare.index.duplicated(keep='last')]
+    
+        # บันทึกลง result
+        result = pd.concat([result, predict_test_1], axis=0)
+    #------------TEMP PREDICT VALUE--------------
+    
+    real_data = data_filtered.loc[start_date:end_date]
+    # สมมุติว่า result คือ DataFrame ที่รวม predictions กับ pm_10
+    result_plot_temp = pd.concat([result['prediction_label'].shift(-1), real_data['temperature']], axis=1)
+    result_plot_temp.columns = ['Predictions', 'Actual TEMP']  # ตั้งชื่อคอลัมน์
+    
+    #---------------------------------------------
+    
+    model_humi = load_model("main/humi_use_temp_model_2")
+    # อ่านไฟล์ CSV
+    data_humi = data.copy()
+    
+    data_humi.drop(columns=['pm_2_5'], inplace=True)
+    
+    # ลบแถวล่าสุด (แถวสุดท้าย)
+    data_humi = data_humi.drop(data_humi.index[-1])
+    data_humi
+    last_date_humi = data_humi.index[-1].strftime('%Y-%m-%d')
+    # add temp predict column
+    shifted_predictions_temp = result['prediction_label'].shift(-1).iloc[:-1]
+    
+    data_humi.loc[shifted_predictions_temp.index, 'temperature'] = shifted_predictions_temp
+    
+    lag_size = 7
+    for i in range(1,lag_size+1):
+        data_humi[f'humidity_lag_{i}'] = data_humi['humidity'].shift(i)
+    
+    for i in range(1,lag_size+1):
+        data_humi[f'temperature_lag_{i}'] = data_humi['temperature'].shift(i)
+        
+    data_filtered = data_humi.loc['2023-09':]  # ดึงข้อมูลตั้งแต่กันยายน 2023 เป็นต้นไป
+    data_compare = data_filtered.loc[:'2025-01-12']
+    result_humi = pd.DataFrame()
+    
+    start_date = "2025-01-13"
+    end_date = last_date_humi
+    
+    model = model_humi
+    
+    for date in pd.date_range(start=start_date, end=end_date, freq="D"):
+        # Drop columns
+        test_1 = data_filtered
+        # ดึงวันนี้มา
+        test_1 = test_1.loc[str(date.date())]
+    
+        # ใส่เข้าไป
+        data_compare = pd.concat([data_compare, test_1], axis=0)
+    
+        for i in range(1,lag_size+1):
+            data_compare[f'humidity_lag_{i}'] = data_compare['humidity'].shift(i)
+    
+        # ดึง
+        test_1 = (data_compare.loc[str(date.date())]).drop(columns=['humidity'])
+        
+        # Drop ทิ้งล่าสุดใน data_compare
+        data_compare = data_compare.drop(data_compare.last_valid_index())
+        test_1 = test_1[~test_1.index.duplicated(keep='last')]
+    
+        # Predict
+        predict_test_1 = predict_model(model, data=test_1)
+    
+        # เพิ่มผลทำนายเข้าไปใน data_compare
+        data_compare = pd.concat([data_compare, predict_test_1], axis=0)
+    
+        # แทนที่ค่าในคอลัมน์ 'pm_2_5' ในวันที่ '2025-01-13' ด้วยค่าในคอลัมน์ 'prediction_label'
+        data_compare.loc[str(date.date()), 'humidity'] = data_compare.loc[str(date.date()), 'prediction_label']
+    
+        # ลบคอลัมน์ 'prediction_label' (หากต้องการแค่ผลทำนาย)
+        data_compare = data_compare.drop(columns=['prediction_label'])
+    
+        # กรองเอาวันที่ซ้ำกัน (index ซ้ำ) ออกให้เหลือแค่ตัวเดียว
+        data_compare = data_compare[~data_compare.index.duplicated(keep='last')]
+    
+        # บันทึกลง result_humi
+        result_humi = pd.concat([result_humi, predict_test_1], axis=0)
+
+    # คำนวณวัน
+    from datetime import datetime, timedelta
+    
+    # แปลง str เป็น datetime
+    date_str = "2025-01-13"
+    date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+    
+    # บวกวัน
+    days_to_add = days - 1  # จำนวนวันที่ต้องการบวก
+    new_date = date_obj + timedelta(days=days_to_add)
+    
+    # แสดงผลลัพธ์
+    print(new_date.strftime("%Y-%m-%d"))
+
+    #-----------PLOT GRAPH HUMI---------
+    real_data = data_filtered.loc[start_date:end_date]
+    # สมมุติว่า result คือ DataFrame ที่รวม predictions กับ pm_10
+    result_plot_humi = pd.concat([result_humi['prediction_label'].shift(-1)[start_date:new_date], real_data['humidity']], axis=1)
+    result_plot_humi.columns = ['Predictions', 'Actual HUMI']  # ตั้งชื่อคอลัมน์
+    #------------------------------------
+    return result_plot_humi
